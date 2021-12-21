751f2d6e938ff07ff493f261e2b71bde54901eb6
CarterTS <carterplasek@gmail.com>
2021-12-21 16:33:05 -0600

    femto step 6: atexit, flush tty

--- Utils/femto/src/main.c
+++ Utils/femto/src/main.c
@@ -1,22 +1,30 @@
+#include <libc/stdlib.h>
 #include <libc/termios.h>
 #include <libc/unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode()
 {
-  struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO);
 
-  raw.c_lflag &= ~(ECHO);
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main()
 {
-  enableRawMode();
-  
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-  return 0;
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
 }
