e9ced297c05ce3cfb7a6ef2bdb2ca87276c8bf5c
eyethan <ethandanielhunt@gmail.com>
2022-02-21 12:24:15 -0500

    add day of the word and swap load to function

--- Games/qordle/src/main.c
+++ Games/qordle/src/main.c
@@ -1,45 +1,72 @@
 #include <libc/stdio.h>
-#include <libc/string.h>
-#include <libc/time.h>
 #include <libc/stdlib.h>
+#include <libc/time.h>
+
+#define ACTUAL_WORD_LIST "/usr/assets/act.csv"
+#define ACCEPTABLE_ANSWER_LIST "/usr/assets/acc.csv"
+
+char *load(char *file);
 
 int main(int argc, char **argv)
 {
+        char *buffer1 = load(ACTUAL_WORD_LIST);
+        char *buffer2 = load(ACCEPTABLE_ANSWER_LIST);
 
-	FILE *fp = fopen("/usr/assets/acc.csv", "rb");
-	if (!fp)
-	{
-    		perror("fopen() failed.");
-    		return(1);
-	}
+        time_t seconds = time(NULL);
 
-	int length = fseek(fp , 0L , SEEK_END);
-	if (length < 0)
-	{
-    		perror("seek failed");
-    		fclose(fp);
-    		return(1);
-	}
-	fseek(fp, 0, SEEK_SET);
+        printf("Word of the day: ");
+        for(int i = 0; i < 5; i++)
+        {
+                if(buffer1[seconds/86400%2309*6+i] == '\n')
+                {
+                        printf("N");
+                }
+                else
+                {
+                        printf("%c", buffer1[seconds/86400%2309*6+i]);
+                }
+        }
+        printf("\n");
 
-	char *buffer = malloc(length + 1);
-	if (!buffer)
-	{
-    		fclose(fp);
-    		eprintf("Allocation Failed\n");
-    		return(1);
-	}
+        free(buffer1);
+        free(buffer2);
+        return 0;
+}
 
-	if (fread(buffer, length, 1, fp) != 1)
-	{
-		perror("seek failed");
-    		fclose(fp);
-    		return(1);
-	}
+char *load(char *file)
+{
+        FILE *fp = fopen(file, "rb");
+        if(!fp)
+        {
+                perror("fopen() failed");
+                exit(1);
+        }
 
-	// This is where you do your thing with the buffer
+        int length = fseek(fp, 0L, SEEK_END);
+        if(length < 0)
+        {
+                perror("seek failed");
+                fclose(fp);
+                exit(1);
+        }
+        fseek(fp, 0, SEEK_SET);
 
-	fclose(fp);
-	free(buffer);
-	return 0;
-}	
+        char *buffer = malloc(length + 2);
+        if (!buffer)
+        {
+                fclose(fp);
+                eprintf("Allocation failed.\n");
+                exit(1);
+        }
+
+        if(fread(buffer, length, 1, fp) != 1)
+        {
+                perror("fread() failed.");
+                fclose(fp);
+                exit(1);
+        }
+
+        printf("%i\n", length);
+        fclose(fp);
+        return buffer;
+}
